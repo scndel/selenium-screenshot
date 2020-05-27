be830c9f09a76e221116428870aa683e48e25685
Stéphane Delval <scndeldev@gmail.com>
2020-05-28 00:16:17 +0200

    exported function with base64 output

--- screenshot.js
+++ screenshot.js
@@ -1,24 +1,16 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
-const util = require('util');
-const fs = require('fs');
 
-const FILENAME = 'screenshot.png'
-const URL_HUB = process.env.URL_HUB;
-const URL_TEST= process.env.URL_TEST;
+async function takeScreenshot(url_hub,url_test) {
 
-const writeFile = util.promisify(fs.writeFile)
+let driver = await new Builder().forBrowser('chrome').usingServer(url_hub).build();
 
-async function takeScreenshot() {
-
-let driver = await new Builder().forBrowser('chrome').usingServer(URL_HUB).build();
-
-await driver.get(URL_TEST);
+await driver.get(url_test);
 
 let image = await driver.takeScreenshot();
 
-await writeFile(FILENAME, image, 'base64');
-
 await driver.quit();
 
+return image;
 }
-takeScreenshot()
+
+exports.takeScreenshot = takeScreenshot;
